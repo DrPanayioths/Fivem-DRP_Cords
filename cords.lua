5208bff2e73254361b96436bade6b01a501d0fab
DrPanayioths <panayioths0072019@gmail.com>
2025-04-07 22:58:27 +0300

    Perm Level Command Usability
    You Can Set To Only Your admins to be able to use the command

--- cords.lua
+++ cords.lua
@@ -1,11 +1,40 @@
 -- Set Dev_Status To "enable" Or "disable"
 local Dev_status = "enable"
+local Perm_Level = "admin"  -- Set To "admin" or "everyone"
 
-if Dev_Status == "enable" then
-    RegisterCommand("gc", function()
-        local playerPed = PlayerPedId()
-        local coords = GetEntityCoords(playerPed)
-        print("Your coordinates: " .. tostring(coords))
-    end, false)
-elseif Dev_Status == "disable" then
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- DONT EDIT Below That Point
+if Dev_status == "enable" then
+    if Perm_Level == "admin" then
+        Player = exports["es_extended"]:getSharedObject()
+        RegisterCommand("gc", function()
+            if Player.PlayerData.group == 'admin' then
+                local playerPed = PlayerPedId()
+                local coords = GetEntityCoords(playerPed)
+                print("Your coordinates: " .. tostring(coords))
+            else
+                print("Not Allowed To Use That Command")
+            end
+        end, false)
+    
+    elseif Perm_Level == "everyone" then
+        RegisterCommand("gc", function()
+            local playerPed = PlayerPedId()
+            local coords = GetEntityCoords(playerPed)
+            print("Your coordinates: " .. tostring(coords))
+        end, false)
+    end
 end
